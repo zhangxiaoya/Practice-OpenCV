61635bb7caf2cdf95a8b2e9a6d84834d0cd7494a
zhangxiaoya <tiyijushi@163.com>
2017-01-20 02:35:14 +0800

    [N/A][yanan] visit image by pixel

--- ROI/Main.cpp
+++ ROI/Main.cpp
@@ -17,5 +17,28 @@ int main()
 	imshow("Original image with rectangle", imgClone);
 	imshow("ROI", roi);
 	waitKey(0);
+
+	Mat grayImg = Mat(300, 400, CV_8U);
+	for (int i = 0; i < grayImg.rows;++i)
+	{
+		for (int j = 0; j < grayImg.cols;++j)
+		{
+			grayImg.at<uchar>(i, j) = (i + j) % 255;
+		}
+	}
+	imshow("grayImg", grayImg);
+	waitKey(0);
+	Mat rgbImg = Mat(300, 400, CV_8UC3);
+	for (int i = 0; i < rgbImg.rows;++i)
+	{
+		for (int j = 0; j < rgbImg.cols;++j)
+		{
+			rgbImg.at<Vec3b>(i, j)[0] = i % 255;
+			rgbImg.at<Vec3b>(i, j)[1] = j % 255;
+			rgbImg.at<Vec3b>(i, j)[2] = 0;
+		}
+	}
+	imshow("RGB image", rgbImg);
+	waitKey(0);
 	return 0;
 }
