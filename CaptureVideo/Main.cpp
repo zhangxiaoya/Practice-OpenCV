4ebee8b126dd5bb8671ee63e11ee70e8c37f06ce
zhangxiaoya <tiyijushi@163.com>
2017-01-22 02:04:26 +0800

    [N/A][yanan] use camera of your laptop

---
+++ CaptureVideo/Main.cpp
@@ -0,0 +1,26 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	VideoCapture cap(0);
+	if (!cap.isOpened())
+	{
+		cout << "Can not open camera" << endl;
+		return -1;
+	}
+	Mat frame;
+	Mat edges;
+	bool isBreakByUser = false;
+	while (!isBreakByUser)
+	{
+		cap >> frame;
+		imshow("Current Video", frame);
+		if (waitKey(30) >= 0)
+			isBreakByUser = true;
+	}
+	return 0;
+}
