c8f3671d32de74d6bab154c1ebd98d4775d6f425
zhangxiaoya <tiyijushi@163.com>
2017-02-11 00:19:01 +0800

    [yanna][N/A] add positive filter

--- Filter/Main.cpp
+++ Filter/Main.cpp
@@ -11,7 +11,7 @@ int main()
 		cout << "Error: can not read file!";
 	}
 	cv::Mat sculptureResultImageWithThreeDegree(sourceImage.size(), CV_8UC3);
-	cv::Mat sculptureResultImageWithTwoDegree(sourceImage.size(), CV_8UC3);
+	cv::Mat sculptureResultImageWithThreeDegreeMore(sourceImage.size(), CV_8UC3);
 
 	cv::imshow("Source Image", sourceImage);
 
@@ -21,6 +21,7 @@ int main()
 		uchar* nextRow = sourceImage.ptr<uchar>(i+1);
 
 		uchar* curRow = sculptureResultImageWithThreeDegree.ptr<uchar>(i);
+		uchar* curRowMore = sculptureResultImageWithThreeDegreeMore.ptr<uchar>(i);
 
 		for (int j = 1; j < sourceImage.cols - 1;++j)
 		{
@@ -29,16 +30,25 @@ int main()
 			{
 				channel--;
 				int res = nextRow[3 * (j + 1) + channel] - preRow[3 * (j - 1) + channel] + 128;
+
 				if (res > 255)
 					res = 255;
 				else if (res < 0)
 					res = 0;
 				curRow[3 * j + channel] = res;
+
+				int resMore = preRow[3 * (j + 1) + channel] - nextRow[3 * (j - 1) + channel] + 128;
+				if (resMore > 255)
+					resMore = 255;
+				else if (resMore < 0)
+					resMore = 0;
+				curRowMore[3 * j + channel] = resMore;
 			}
 		}
 	}
 
 	cv::imshow("Filter Image", sculptureResultImageWithThreeDegree);
+	cv::imshow("Filter Image More", sculptureResultImageWithThreeDegreeMore);
 	cv::waitKey();
 	return 0;
 }
