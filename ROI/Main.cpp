46818f11a5a69e13ff687710ba0171b3ce4920b8
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-07-12 17:02:19 +0800

    check performance when generate an rgb image use ptr

--- ROI/Main.cpp
+++ ROI/Main.cpp
@@ -62,6 +62,20 @@ void GenerateGrayImageUsePtr(cv::Mat& grayImg)
 	}
 }
 
+void GenerateRGBImageUsePtr(cv::Mat& rgbImg)
+{
+	for (auto i = 0; i < rgbImg.rows; ++i)
+	{
+		auto p = rgbImg.ptr<cv::Vec3b>(i);
+		for (auto j = 0; j < rgbImg.cols; ++j)
+		{
+			p[j][0] = (i + j) % 255;
+			p[j][1] = (i + j) % 255;
+			p[j][2] = 0;
+		}
+	}
+}
+
 int main()
 {
 	auto img = cv::imread(".\\lena.png");
@@ -86,50 +100,44 @@ int main()
 	imshow("ROI", roi);
 	cv::waitKey(0);
 
+	auto height = img.rows;
+	auto width = img.cols;
+
 	// visit image the first methd -- use at operator
-	auto grayImg = cv::Mat(300, 400, CV_8U);
+	auto grayImg = cv::Mat(height, width, CV_8U);
 	CheckPerf(GenerateGrayImageUseAtOperator(grayImg), "GenerateGrayImageUseAtOperator");
 	imshow("grayImg", grayImg);
 	cv::waitKey(0);
 
-	auto rgbImg = cv::Mat(300, 400, CV_8UC3);
+	auto rgbImg = cv::Mat(height, width, CV_8UC3);
 	CheckPerf(GenerateRGBImageUseAtOperator(rgbImg), "GenerateRGBImageUseAtOperator");
 	imshow("RGB image", rgbImg);
 	cv::waitKey(0);
 
 	// visit iamge the second method -- use mat iterator
-	auto grayImg2 = cv::Mat(300, 400, CV_8U);
+	auto grayImg2 = cv::Mat(height, width, CV_8U);
 	CheckPerf(GenerateGrayImageUseMatIterator(grayImg2), "GenerateGrayImageUseMatIterator");
 	imshow("Gray Image 2", grayImg2);
 	cv::waitKey(0);
 
-	auto rgbImge2 = cv::Mat(300, 400, CV_8UC3);
+	auto rgbImge2 = cv::Mat(height, width, CV_8UC3);
 	CheckPerf(GenerateRGBImageUseMatIterator(rgbImge2), "GenerateRGBImageUseMatIterator");
 	imshow("RGB Image 2", rgbImge2);
 	cv::waitKey();
 
 	// visit iamge the third method -- use ptr point
-	auto grayImg3 = cv::Mat(300, 400, CV_8U);
+	auto grayImg3 = cv::Mat(height, width, CV_8U);
 	CheckPerf(GenerateGrayImageUsePtr(grayImg3), "GenerateGrayImageUsePtr");
 	imshow("Gray Image 3", grayImg3);
 	cv::waitKey();
 
-	auto rgbImg3 = cv::Mat(300, 400, CV_8UC3);
-	for (auto i = 0; i < rgbImg3.rows; ++i)
-	{
-		auto p = rgbImg3.ptr<cv::Vec3b>(i);
-		for (auto j = 0; j < rgbImg3.cols; ++j)
-		{
-			p[j][0] = (i + j) % 255;
-			p[j][1] = (i + j) % 255;
-			p[j][2] = 0;
-		}
-	}
+	auto rgbImg3 = cv::Mat(height, width, CV_8UC3);
+	CheckPerf(GenerateRGBImageUsePtr(rgbImg3), "GenerateRGBImageUsePtr");
 	imshow("RGB Image 3", rgbImg3);
 	cv::waitKey();
 
 	// use step
-	auto grayImg4 = cv::Mat(300, 400, CV_8U, cv::Scalar(0));
+	auto grayImg4 = cv::Mat(height, width, CV_8U, cv::Scalar(0));
 	for (auto i = 0; i < grayImg4.rows; ++i)
 	{
 		for (auto j = 0; j < grayImg4.cols; ++j)
