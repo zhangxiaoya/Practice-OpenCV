0214a78b7fe3acb57edb206133f395d0302f33ce
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-07-13 10:13:16 +0800

    refactor and add ml lib

--- KNN/Main.cpp
+++ KNN/Main.cpp
@@ -1,14 +1,13 @@
-﻿#include <opencv2/opencv.hpp>
+﻿#include <ctime>
+#include <cstdio>
 #include <iostream>
-#include <ctime>
+#include <ml/ml.hpp>
+#include <contrib/contrib.hpp>
 
-using namespace  std;
-using namespace cv;
-
-int readFlippedInteger(FILE *fp) {
+int readFlippedInteger(FILE* fp)
+{
 	auto ret = 0;
-	uchar *temp;
-	temp = reinterpret_cast<uchar*>(&ret);
+	auto temp = reinterpret_cast<uchar*>(&ret);
 	fread(&temp[3], sizeof(uchar), 1, fp);
 	fread(&temp[2], sizeof(uchar), 1, fp);
 	fread(&temp[1], sizeof(uchar), 1, fp);
@@ -18,51 +17,54 @@ int readFlippedInteger(FILE *fp) {
 
 int main()
 {
-	FILE *fp = fopen("..\\data\\train-images.idx3-ubyte", "rb");
+	FILE* fp;
+	FILE* fp2;
+	fopen_s(&fp, "..\\data\\train-images.idx3-ubyte", "rb");
 
-	FILE *fp2 = fopen("..\\data\\train-labels.idx1-ubyte", "rb");
+	fopen_s(&fp2, "..\\data\\train-labels.idx1-ubyte", "rb");
 
 	if (!fp || !fp2)
 	{
-		//	cout << "Files not Found" << endl;
-		return 0;
+		std::cout << "Files Not Found" << std::endl;
+		system("Pause");
+		return -1;
 	}
 
-	int magicNumber = readFlippedInteger(fp);
-	int numImages = readFlippedInteger(fp);
-	int numRows = readFlippedInteger(fp);
-	int numCols = readFlippedInteger(fp);
+	auto magicNumber = readFlippedInteger(fp);
+	auto numImages = readFlippedInteger(fp);
+	auto numRows = readFlippedInteger(fp);
+	auto numCols = readFlippedInteger(fp);
 
 	fseek(fp2, 0x08, SEEK_SET);
 
-	int size = numRows*numCols;
+	auto size = numRows * numCols;
 
-	Mat trainingVectors(numImages, size, CV_32FC1);
-	Mat trainingLabels(numImages, 1, CV_32FC1);
+	cv::Mat trainingVectors(numImages, size, CV_32FC1);
+	cv::Mat trainingLabels(numImages, 1, CV_32FC1);
 	//CvMat *trainingVectors = cvCreateMat(numImages, size, CV_32FC1);
 	//CvMat *trainingLabels = cvCreateMat(numImages, 1, CV_32FC1);
 
-	uchar *temp = new uchar[size];
+	auto temp = new uchar[size];
 	//unsigned char *temp = new unsigned char[size];
 
 	uchar tempClass = 0;
 
-	for (int i = 0; i < numImages; i++)
+	for (auto i = 0; i < numImages; i++)
 	{
-		fread((void*)temp, size, 1, fp);
-		fread((void*)(&tempClass), sizeof(uchar), 1, fp2);
+		fread(static_cast<void*>(temp), size, 1, fp);
+		fread(static_cast<void*>(&tempClass), sizeof(uchar), 1, fp2);
 		trainingLabels.at<float>(i, 0) = tempClass;
-		Mat img(numRows, numCols, CV_32FC1);
-		for (int k = 0; k < size; k++)
+		cv::Mat img(numRows, numCols, CV_32FC1);
+		for (auto k = 0; k < size; k++)
 		{
 			trainingVectors.at<float>(i, k) = temp[k];
-			img.at<float>(k / numCols, k%numCols) = temp[k];
+			img.at<float>(k / numCols, k % numCols) = temp[k];
 		}
-//		imshow("data", img);
+		//		imshow("data", img);
 		//waitKey(2);
 	}
 
-	KNearest knn(trainingVectors, trainingLabels);
+	cv::KNearest knn(trainingVectors, trainingLabels);
 	printf("Maximum k: %d\n", knn.get_max_k());
 
 
@@ -71,8 +73,8 @@ int main()
 	delete[] temp;
 
 
-	fp = fopen("..\\data\\t10k-images.idx3-ubyte", "rb");
-	fp2 = fopen("..\\data\\t10k-labels.idx1-ubyte", "rb");
+	fopen_s(&fp, "..\\data\\t10k-images.idx3-ubyte", "rb");
+	fopen_s(&fp2, "..\\data\\t10k-labels.idx1-ubyte", "rb");
 
 	magicNumber = readFlippedInteger(fp);
 	numImages = readFlippedInteger(fp);
@@ -81,27 +83,26 @@ int main()
 	numCols = readFlippedInteger(fp);
 
 	fseek(fp2, 0x08, SEEK_SET);
-	CvMat *testVectors = cvCreateMat(numImages, size, CV_32FC1);
-	CvMat *testLabels = cvCreateMat(numImages, 1, CV_32FC1);
-	CvMat *actualLabels = cvCreateMat(numImages, 1, CV_32FC1);
+	auto testVectors = cvCreateMat(numImages, size, CV_32FC1);
+	auto testLabels = cvCreateMat(numImages, 1, CV_32FC1);
+	auto actualLabels = cvCreateMat(numImages, 1, CV_32FC1);
 	temp = new uchar[size];
 	tempClass = 1;
-	CvMat *currentTest = cvCreateMat(1, size, CV_32FC1);
-	CvMat *currentLabel = cvCreateMat(1, 1, CV_32FC1);
-	int totalCorrect = 0;
+	auto currentTest = cvCreateMat(1, size, CV_32FC1);
+	auto currentLabel = cvCreateMat(1, 1, CV_32FC1);
+	auto totalCorrect = 0;
 
-	for (int i = 0; i<numImages; i++)
+	for (auto i = 0; i < numImages; i++)
 	{
+		fread(static_cast<void*>(temp), size, 1, fp);
 
-		fread((void*)temp, size, 1, fp);
+		fread(static_cast<void*>(&tempClass), sizeof(uchar), 1, fp2);
 
-		fread((void*)(&tempClass), sizeof(uchar), 1, fp2);
+		actualLabels->data.fl[i] = static_cast<float>(tempClass);
 
-		actualLabels->data.fl[i] = (float)tempClass;
-
-		for (int k = 0; k<size; k++)
+		for (auto k = 0; k < size; k++)
 		{
-			testVectors->data.fl[i*size + k] = temp[k];
+			testVectors->data.fl[i * size + k] = temp[k];
 			currentTest->data.fl[k] = temp[k];
 		}
 		knn.find_nearest(currentTest, 5, currentLabel);
@@ -111,7 +112,8 @@ int main()
 		if (currentLabel->data.fl[0] == actualLabels->data.fl[i])
 			totalCorrect++;
 	}
-	printf("Time: %d Accuracy: %f ", (int)time, (double)totalCorrect * 100 / (double)numImages);
-	int t;
-	cin >> t;
+	printf("Time: %d Accuracy: %f ", reinterpret_cast<int>(time), static_cast<double>(totalCorrect) * 100 / static_cast<double>(numImages));
+
+	system("Pause");
+	return 0;
 }
