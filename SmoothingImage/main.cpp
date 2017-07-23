c9588e182fc3c91fc1851e7b7508348a1cc13e84
ynzhang <ynzhang.vis@gmail.com>
2017-07-23 14:52:17 +0800

    add snapshow method

--- SmoothingImage/main.cpp
+++ SmoothingImage/main.cpp
@@ -3,18 +3,25 @@
 #include <imgproc/imgproc.hpp>
 #include <iostream>
 
+void SnapShow(const cv::Mat& img, const char* winname)
+{
+	cv::namedWindow(winname);
+	imshow(winname, img);
+	cv::waitKey(0);
+	cv::destroyWindow(winname);
+}
+
 int main(int argc, char* argv[])
 {
 	auto img = cv::imread("..\\data\\lena.png");
-	if(img.empty())
+	if (img.empty())
 	{
-		std::cout << "Read Image File Failed!"<<std::endl;
+		std::cout << "Read Image File Failed!" << std::endl;
 		system("Pause");
 		return -1;
 	}
-	cv::imshow("current Image", img);
-	cv::waitKey(0);
 
+	SnapShow(img, "Original Image");
 	cv::destroyAllWindows();
 	system("Pause");
 	return 0;
