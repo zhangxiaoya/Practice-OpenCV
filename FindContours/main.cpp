eef36fdf0dcda52091a3f715923a26342c95a34c
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-08-02 14:46:14 +0800

    add an example for find contours

--- FindContours/main.cpp
+++ FindContours/main.cpp
@@ -1,18 +1,43 @@
-#include <iostream>
-#include <highgui/highgui.hpp>
+#include "cv.h"
+#include "highgui.h"
 
-int main(int argc, char* argv[])
+using namespace cv;
+
+int main(int argc, char** argv)
 {
-	auto img = cv::imread("..\\data\\lena.png");
-	if(img.empty() == true)
+	char fullName[100];
+	std::string forMat = "E:\\WorkLogs\\Frame_%02d.png";
+	auto count = 18;
+
+	for (auto i = 0; i < count; ++i)
 	{
-		std::cout << "Read Image File Failed!" << std::endl;;
-		system("Pause");
-		return -1;
+		sprintf_s(fullName, 100, forMat.c_str(),i);
+		auto src = imread(fullName, 0);
+
+		Mat dst = Mat::zeros(src.rows, src.cols, CV_8UC3);
+
+		namedWindow("Source", 1);
+		imshow("Source", src);
+
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+
+		findContours(src, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+		if(contours.size() == 0)
+			continue;
+
+		for (auto idx = 0; idx >= 0; idx = hierarchy[idx][0])
+		{
+			Scalar color(rand() & 255, rand() & 255, rand() & 255);
+			drawContours(dst, contours, idx, color, CV_FILLED, 8, hierarchy);
+		}
+
+		namedWindow("Components", 1);
+		imshow("Components", dst);
+		waitKey(1000);
 	}
-	imshow("Original Image", img);
-	cv::waitKey(0);
-	cv::destroyAllWindows();
-	system("Pause");
+
+	destroyAllWindows();
 	return 0;
 }
