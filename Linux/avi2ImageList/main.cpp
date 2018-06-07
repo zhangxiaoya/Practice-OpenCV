65bb5cf4a23c326c4cf95f15d1faa653946ff8e1
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-06-07 15:21:01 +0800

    Update

--- Linux/avi2ImageList/main.cpp
+++ Linux/avi2ImageList/main.cpp
@@ -4,35 +4,37 @@
 #include <opencv2/highgui.hpp>
 #include <iomanip>
 
-int main()
+int main(int argc, char **argv)
 {
-    const char* imageListFolder = "/home/runisys/Downloads/text/";
+    if (argc != 3)
+    {
+        std::cout << "Usage: \n \t" << argv[0] << " image_folder/ avi_file_name" << std::endl;
+        return 1;
+    }
+    const char *imageListFolder = argv[1];
     cv::VideoCapture capture;
-    capture.open("/home/runisys/Downloads/text.avi");
+    capture.open(argv[2]);
+    if (!capture.isOpened())
+    {
+        std::cout << "Open AVI file failed!" << std::endl;
+        return 2;
+    }
     cv::Mat frame;
 
-
     int frameId = 0;
-    if(capture.isOpened())
+    while (true)
     {
-        while (true)
-        {
-            capture >> frame;
-            if(frame.empty())
-                break;
-            std::stringstream ss;
-            std::cout << std::setw(6) << frameId << std::endl;
-            cv::imshow("Frame", frame);
-            ss << imageListFolder << std::setw(6) << std::setfill('0') << frameId << ".png";
-            std::cout << "filename is " << ss.str()<<std::endl;
-            cv::imwrite(ss.str(), frame);
-            cv::waitKey(100);
-            ++frameId;
-        }
-    }
-    else
-    {
-        std::cout << "Unable open avi files" << std::endl;
+        capture >> frame;
+        if (frame.empty())
+            break;
+        std::stringstream ss;
+        std::cout << std::setw(6) << frameId << std::endl;
+        cv::imshow("Frame", frame);
+        ss << imageListFolder  << "/" << std::setw(6) << std::setfill('0') << frameId << ".png";
+        std::cout << "filename is " << ss.str() << std::endl;
+        cv::imwrite(ss.str(), frame);
+        cv::waitKey(100);
+        ++frameId;
     }
 
     cv::destroyAllWindows();
